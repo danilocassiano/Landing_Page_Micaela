c11bd0453394f3c89cd4b74ae8c215a170e9cb6a
Alison Simião <alison.s@multiplier.com.br>
2023-12-20 23:34:20 -0300

    fix(homepage): create efect image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import HomePage from './pages/Homepage';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <ChakraProvider>
       <CSSReset />
+      <Header />
       <HomePage />
     </ChakraProvider>
   );
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,40 +1,50 @@
-import { Box, Image } from "@chakra-ui/react";
-import Header from "../../components/Header";
+import { Box, Image, Text } from "@chakra-ui/react";
 import Whatsapp from "../../components/Whatsapp";
-import Email from "../../components/Email";
 
 export default function HomePage() {
 
     return (
         <Box
             maxW='100vw'
+            maxH={'80vh'}
+            overflow={'hidden'}
         >
-            <Header />
-            <Box p={4} bg="#A52A2A" h='100vh' position="relative">
+            <Box bg="#A52A2A">
                 <Box display="flex" alignItems="center" justifyContent="space-between" height="100%">
                     <div>
-                        <h1>Precisa de Ajuda pra enfrentar <br /> as dificuldades da Vida?</h1>
-                        <p>Olá, Sou Micaela Santos, psicóloga.</p>
-                        <p>
-                            <strong>
-                                Vamos conversar?
-                            </strong>
-                        </p>
+                        <Text fontFamily="Calisto MT" fontSize='2rem' marginBottom='8px' color="white">
+                            Precisa de Ajuda pra enfrentar <br /> as dificuldades da Vida?
+                        </Text>
+                        <Text fontSize="1.2rem" fontFamily="Poppins, sans-serif" marginBottom="8px">
+                            Olá, sou Micaela Santos, psicóloga.
+                        </Text>
+                        <Text fontSize="1.2rem" fontFamily="Poppins, sans-serif" marginBottom="16px">
+                            <strong>Vamos conversar?</strong>
+                        </Text>
 
                         <Whatsapp />
                     </div>
-
-                    <Image
-                        src={"/IMG-apresentacao.png"}
-                        alt="Imagem de Apresentação"
-                        backgroundColor="transparent"
-                        
-                        h="auto"
+                    <Box
+                        w="60%"
+                        position='relative'
+                        _after={{
+                            content: '""',
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            top: '0px',
+                            left: '0px',
+                            bgGradient: 'linear(to right, #a52a2a, transparent)',
+                        }}
+                    >
+                        <Image
+                            width={'100%'}
+                            src={"/IMG_apresentacao.png"}
+                            alt="Imagem de Apresentação"
                         />
+                    </Box>
                 </Box>
             </Box>
-            <Email />
-            
         </Box>
     );
 };
