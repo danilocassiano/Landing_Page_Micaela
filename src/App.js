8d4f115a66833c24d889528caa169d82bdebe83b
Alison simiao <alison.simiao@gmail.com>
2024-01-10 12:00:44 -0300

    feat(cuidados): criação de lista

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/Homepage';
 import Vantagens from './pages/Vantagens';
 import Modalidades from './pages/Modalidades';
 import Header from './components/Header';
+import Cuidados from './pages/Cuidados';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <HomePage />
       <Vantagens />
       <Modalidades />
+      <Cuidados />
     </ChakraProvider>
   );
 }
---
+++ src/pages/Cuidados/index.jsx
@@ -0,0 +1,69 @@
+import { Center, HStack, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+import React from 'react';
+import Whatsapp from '../../components/Whatsapp';
+import { FaCheck } from "react-icons/fa";
+
+function Cuidados() {
+    const content = [
+        'Depressão', 
+        'transtorno de personalidade', 
+        'ansiedade', 
+        'transtorno de humor',
+        'suicidio',
+
+'insegurança',
+'bipolaridade',
+'timidez',
+'medo',
+'disfunções sexuais',
+'dependência quimica',
+'distúbios do sono',
+'estresse pós-traumático',
+'baixa autoconfiança',
+'baixa autoestima',
+'relacionaento abusivo',
+'ciúmes',
+'traumas do passado'
+
+    ]
+
+  return ( 
+  <Center
+    w='100vw'
+    h='100vh'
+    flexDir='column'
+    color='white'
+    pd='5rem'
+    gap={'5rem'}
+    bgGradient= 'linear(to right, #a52a2a, #a52)'
+    >
+    <Heading
+        fontFamily="Poppins"
+    > o que a terapia pode cuidar ?</Heading>
+
+    <SimpleGrid 
+        templateColumns={'1fr 1fr'}
+        columnGap='10'
+        rowGap='5'
+        w='60%'
+    >
+    {
+        content.map(sick => 
+            (
+                <HStack
+                   borderBottom='1px solid rgba(255   ,255   ,255   ,0.1)'
+                   pb='.3rem'
+
+                >
+                    <FaCheck /><Text>{sick[0].toUpperCase() + sick.substring(1)}</Text>
+                </HStack>
+            )
+            )
+    }
+    </SimpleGrid>
+    <Whatsapp />
+  </Center>
+  );
+}
+
+export default Cuidados;
